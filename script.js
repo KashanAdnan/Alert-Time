dbd08d47eabfe9986b17d7f04e600d1af1bbf7f5
KashanAdnan <khanzaidaboy@gmail.com>
2023-05-14 14:06:51 +0500

    some-code

---
+++ script.js
@@ -0,0 +1,15 @@
+function displayHour() {
+  var date = new Date();
+  var hours = date.getHours();
+  alert(hours);
+}
+
+function displayMinutes() {
+  var date = new Date();
+  var minute = date.getMinutes();
+  alert(minute);
+}
+function displayDate() {
+  var date = new Date();
+  alert(date);
+}
